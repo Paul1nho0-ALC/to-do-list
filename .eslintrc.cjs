798bbfa78d2df072fddf7455ad9b1dc7f9a2e7e6
Paulo Alcântara <pauloooalcantarah@gmail.com>
2023-06-16 18:54:25 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: { browser: true, es2021: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'prettier', 'react', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
